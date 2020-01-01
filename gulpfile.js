ff70cceab3053fcf6d819db493b9ec2dac2e480a
hossein.s <hsee1372@gmail.com>
2020-01-01 17:31:32 +0330

    absoulteRElativeCssSpriteICON

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,17 @@
-var gulp =require("gulp"),
-watch=require('gulp-watch'),
-autoprefixer=require('autoprefixer')
-postcss=require('gulp-postcss')
-cssnested=require('postcss-nested')
-cssvars=require('postcss-simple-vars')
-cssrename=require('postcss-class-rename')
-cssImport = require('postcss-import')
-browserSync = require('browser-sync').create()
+var gulp =require("gulp");
+browserSync = require('browser-sync').create();
+var a =require('./tasks/css');
+var b =require('./tasks/watch');
+exports.css=a.css;
+exports.configBrowser=b.browser;
+exports.reload=b.reloadBrowser;
+exports.inject=b.inject;
 
-;
-
-
-
-
-function css(){
-   
-    return gulp.src('./asset/css/main.css')
-    .pipe(postcss([cssImport,cssvars,cssnested]))
-    .pipe(postcss([autoprefixer]))
-    .pipe(gulp.dest('./'));
-}
-
-
-gulp.task(css);
-
-function html(){
+function watch(){
+//  gulp.series(configBrowser);
+watch(['./asset/css/main.css','./asset/css/**/*.css'],gulp.series(css,inject));
+    watch(['./index.html'],reload)
+    
 
 }
-gulp.task(html);
-// function reload(){
-//     browserSync.reload();
-// }
-// gulp.task(reload);
-// function injectCss(){
-//     return gulp.src('./asset/css/main.css')
-//     .pipe(browserSync.stream());
-// }
-function inject(){
-    return gulp.src('./main.css')
-  .pipe(browserSync.stream());
-}
-gulp.task(inject);
-function watche(){
-    browserSync.init({
-        notify:true,
-        server: {
-            baseDir: "./"
-        }
-    });
-    watch(['./asset/css/main.css','./asset/css/**/*.css'],gulp.series(css,inject));
-    watch(['./index.html'],function(){
-        browserSync.reload();
-    });
-}
-gulp.task(watche);
-
+exports.watch=watch;
